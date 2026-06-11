a6d71728fd11463063d7f3fc7ea69e1e51913e7e
ana-mot <mot_ana@yahoo.com>
2026-06-11 14:48:07 +0300

    read_ok

--- mem_agent/mem_driver.sv
+++ mem_agent/mem_driver.sv
@@ -19,10 +19,7 @@ class mem_driver extends uvm_driver#(mem_transaction);
     	mem_transaction tr;
       	super.run_phase (phase);
 
-	  	vif.drv_cb.mem_sel_en <= 0;
-    	vif.drv_cb.mem_wr_rd_s <= 0;
-    	vif.drv_cb.mem_addr <= 0;
-    	vif.drv_cb.mem_wr_data <= 0;
+	  	
 
       	forever begin
         	seq_item_port.get_next_item (tr);
@@ -45,17 +42,13 @@ class mem_driver extends uvm_driver#(mem_transaction);
 
 		@(vif.drv_cb);
 		vif.drv_cb.mem_wr_data <= tr.wr_data;
-		
-		wait(vif.drv_cb.mem_ack != 4'b0000);
+		vif.drv_cb.mem_sel_en <= 1'b0;
+		@(vif.drv_cb);
 		tr.ack = vif.drv_cb.mem_ack;
 
 		`uvm_info(get_type_name(), $sformatf("WRITE addr=%0h data=%0h ack=%0b", tr.addr, tr.wr_data, tr.ack), UVM_MEDIUM)
 
-		@(vif.drv_cb);
-		vif.drv_cb.mem_sel_en <= 0;
-		vif.drv_cb.mem_wr_rd_s <= 0;
-		vif.drv_cb.mem_addr <= 0;
-		vif.drv_cb.mem_wr_data <= 0;
+		
     endtask
   
     virtual task drive_read(mem_transaction tr);
@@ -65,18 +58,15 @@ class mem_driver extends uvm_driver#(mem_transaction);
 		vif.drv_cb.mem_addr <= tr.addr;
 
 		@(vif.drv_cb);
-
-		wait(vif.drv_cb.mem_ack != 4'b0000);
+		vif.drv_cb.mem_sel_en <= 1'b0;
+		@(vif.drv_cb);
 		tr.rd_data = vif.drv_cb.mem_rd_data;
 		tr.ack = vif.drv_cb.mem_ack;
 	
 
-		`uvm_info(get_type_name(), $sformatf("READ addr=%0h data=%08h ack=%0b", tr.addr, tr.rd_data, tr.ack), UVM_MEDIUM)
+		`uvm_info(get_type_name(), $sformatf("READ addr=%0h data=%0h ack=%0b", tr.addr, tr.rd_data, tr.ack), UVM_MEDIUM)
 
-		@(vif.drv_cb);
-		vif.drv_cb.mem_sel_en <= 0;
-		vif.drv_cb.mem_wr_rd_s <= 0;
-		vif.drv_cb.mem_addr <= 0;
+		
 	endtask
 
 endclass
--- mem_agent/mem_monitor.sv
+++ mem_agent/mem_monitor.sv
@@ -6,7 +6,6 @@ class mem_monitor extends uvm_monitor;
     uvm_analysis_port #(mem_transaction)  mon_analysis_port;
 
     bit [7:0] expected_mem [256];
-    bit [7:0] actual_data;
 
     function new (string name = "mem_monitor", uvm_component parent= null);
       super.new (name, parent);
@@ -24,6 +23,8 @@ class mem_monitor extends uvm_monitor;
 
    virtual task run_phase (uvm_phase phase);
         mem_transaction tr;
+        bit [7:0] actual_data;
+        wait(vif.rst_n == 1'b1);
 
         forever begin
             @(vif.mon_cb);
@@ -34,9 +35,10 @@ class mem_monitor extends uvm_monitor;
                 //WRITE
                 if(vif.mon_cb.mem_wr_rd_s) begin
                     tr.op = mem_transaction::WRITE;
-                    tr.wr_data = vif.mon_cb.mem_wr_data;
+                    
                     wait(vif.mon_cb.mem_ack != 4'b0000);
                     tr.ack = vif.mon_cb.mem_ack;
+                    tr.wr_data = vif.mon_cb.mem_wr_data;
                     expected_mem[tr.addr] = tr.wr_data;
                     `uvm_info(get_type_name(),$sformatf("MONITOR WRITE addr=%0h data=%0h ack=%0b", tr.addr, tr.wr_data, tr.ack), UVM_MEDIUM)
 
@@ -49,19 +51,14 @@ class mem_monitor extends uvm_monitor;
                     tr.ack = vif.mon_cb.mem_ack;
                     tr.rd_data = vif.mon_cb.mem_rd_data;
 
-                    /*case(tr.addr)
-                        0: actual_data = tr.rd_data[7:0];
-                        1: actual_data = tr.rd_data[15:8];
-                        2: actual_data = tr.rd_data[23:16];
-                        3: actual_data = tr.rd_data[31:24];
-                        default: actual_data = 'x;
-                    endcase*/
-
-                    if(expected_mem[tr.addr] !== tr.rd_data[7:0]) begin
-                        `uvm_error(get_type_name(),$sformatf("READ MISMATCH addr=0x%0h expected=0x%0h actual=0x%0h",tr.addr,expected_mem[tr.addr],tr.rd_data[7:0]))
+                    actual_data = tr.rd_data[tr.addr*8 +: 8];    // octetul portului adresat
+                    if(expected_mem[tr.addr] !== actual_data) begin
+                        `uvm_error(get_type_name(), $sformatf("READ MISMATCH addr=0x%0h expected=0x%0h actual=0x%0h", tr.addr, expected_mem[tr.addr], actual_data))
                     end else begin
-                        `uvm_info(get_type_name(),$sformatf("MONITOR READ addr=%0h data=%0h ack=%0b", tr.addr, tr.rd_data, tr.ack), UVM_MEDIUM)
+                        `uvm_info(get_type_name(), $sformatf("READ OK addr=%0h data=%0h ack=%b",tr.addr, actual_data, tr.ack), UVM_MEDIUM)
                     end
+                    //`uvm_info(get_type_name(),$sformatf("MONITOR READ addr=%0h data=%0h ack=%0b", tr.addr, tr.rd_data, tr.ack), UVM_MEDIUM)
+
                     
                end
 
